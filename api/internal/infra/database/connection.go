5f1c88d800d11003a5a60b83e0791e7edddb3d54
Daisuke <kondo151215@gmail.com>
2026-08-11 00:10:27 +0900

    fix: GORMログのSQLパラメータを秘匿する
    DBエラーとslow SQLのログでpasswordやpassword hashの値を補間しないよう、共通GORM設定へParameterizedQueriesを適用する。DryRunテストで機密値が出力されないことを確認する。

--- api/internal/infra/database/connection.go
+++ api/internal/infra/database/connection.go
@@ -1,14 +1,20 @@
 package database
 
 import (
-	"Threadly/internal/domain/models"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+	"time"
+
+	"Threadly/internal/domain/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+const gormSlowThreshold = 200 * time.Millisecond
 
 type DBConfig struct {
 	User     string
@@ -34,13 +40,43 @@ func getDBConfig() DBConfig {
 
 func ConnectionDB() (*gorm.DB, error) {
 	config := getDBConfig()
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=True", config.User, config.Password, config.Host, config.Port, config.DB)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{TranslateError: true})
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?parseTime=True",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.DB,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), NewGORMConfig())
 	if err != nil {
 		return nil, err
 	}
-	if err := db.AutoMigrate(&models.User{}, &models.Post{}); err != nil {
+	if err := autoMigrate(db); err != nil {
 		return nil, err
 	}
 	return db, nil
+}
+
+func NewGORMConfig() *gorm.Config {
+	return &gorm.Config{
+		Logger: newGORMLogger(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			gormSlowThreshold,
+		),
+		TranslateError: true,
+	}
+}
+
+func newGORMLogger(writer logger.Writer, slowThreshold time.Duration) logger.Interface {
+	return logger.New(writer, logger.Config{
+		SlowThreshold:        slowThreshold,
+		Colorful:             false,
+		ParameterizedQueries: true,
+		LogLevel:             logger.Warn,
+	})
+}
+
+func autoMigrate(db *gorm.DB) error {
+	return db.AutoMigrate(&models.User{}, &models.Post{})
 }
